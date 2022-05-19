1c47f004bf7b3aa1ced7c1c0898755366ea79ea6
Rahul Sharma <rahul.sharma@dubswork.com>
2022-05-19 13:38:59 +0530

    Initial Commit

---
+++ MapSwiftUIApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MapSwiftUIApp
+//
+//  Created by Rahul Sharma on 19/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MapSwiftUIApp/MapSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapSwiftUIAppApp.swift
+//  MapSwiftUIApp
+//
+//  Created by Rahul Sharma on 19/05/22.
+//
+
+import SwiftUI
+
+@main
+struct MapSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
